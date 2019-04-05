17e6d5509e9bf20eb9a5db3bf51c40287e8e562e
josemorenoakka <jmorenoj@gmail.com>
2019-04-05 10:50:41 +0200

    Algo01 solved

--- statements/Algo01.cpp
+++ statements/Algo01.cpp
@@ -1,16 +1,23 @@
 /*  C++ Algorithms 1
     
-    Consider the following sequence – 0,1,1,2,3,5,8,13,21,34,55,89,144...
+    Consider the following sequence : 0,1,1,2,3,5,8,13,21,34,55,89,144...
     Write an iterative function to return the nth element of the sequence.
 
 */
 
 #include <iostream>
 
+int fibonacci(int n){
+    if (n == 1) return 0;
+    if (n == 2) return 1;
+    if (n > 2){
+        return fibonacci(n-1) + fibonacci(n-2);
+    }
+}
+
 int sequence(unsigned n)
 {
-    // Smart code goes here
-    return 0;
+    return fibonacci(n);
 }
 
 int main(int argc, char* argv[])
