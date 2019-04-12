5ab5357d79b772cf26cff98076636a4439b9cbb1
josemorenoakka <jmorenoj@gmail.com>
2019-04-12 16:16:04 +0200

    Math04 solved (might be incomplete)

--- statements/Math04.cpp
+++ statements/Math04.cpp
@@ -26,9 +26,35 @@ struct Matrix4x4
     float m[4][4];
 };
 
-struct Transform1
+//We just happend to see wikipedia: https://en.wikipedia.org/wiki/Transformation_matrix#Examples_in_3D_computer_graphics
+//  So we can define this different transforms:
+struct RotationX
 {
-    // ?!?
+    float angle;
+    float rot[3][3];
+    // we should add methods that fill up this matrixes. Like:
+    void generateRotMatrix(){
+        rot[1][1] = 1;
+        /*
+         *
+         */
+        rot[3][3] = cos(angle);
+    }
+};
+
+struct Reflection
+{
+    //symetry plane: ax + by + cz = 0
+    Vec3 plane; //vector normal to the symetry plane: {a, b, c}
+    float ref[3][3];
+    
+    void generateRefMatrix(){
+        ref[1][1] = 1-2*plane.x*plane.x;
+        /*
+         *
+         */
+        ref[3][3] = 1 - 2*plane.z*plane.z;
+    }
 };
 
 int main(int argc, char* argv[])
