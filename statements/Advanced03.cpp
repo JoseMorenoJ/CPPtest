a57ecc6bdcb164f924daaea6aa140527b85def01
josemorenoakka <jmorenoj@gmail.com>
2019-05-31 11:28:15 +0200

    Advanced03.cpp

--- statements/Advanced03.cpp
+++ statements/Advanced03.cpp
@@ -5,6 +5,10 @@
     (And you can't check all the code... That would take too much time). 
     
     Write a comment explaining the steps you will take in order to find the cause of the problem.
+        - I don't have much experience debugging this kinds of problems. Usually I would try to use some debuguing tool
+            to help.
+        - Here I found how someone describes the process he used to debug a similar problem:
+            https://blogs.unity3d.com/2016/04/25/debugging-memory-corruption-who-the-hell-writes-2-into-my-stack-2/
 */
 
 #include <iostream>
