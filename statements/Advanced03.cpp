a67efac99aa859abe7d5a5e8af12b8631e8ecdea
josemorenoakka <jmorenoj@gmail.com>
2019-05-31 10:31:35 +0200

    add Advanced03.cpp

--- statements/Advanced03.cpp
+++ statements/Advanced03.cpp
@@ -1,8 +1,8 @@
 /*  C++ Advanced 3
     
     You are debugging a program using your usual debugger and it always crashes on the same memory address because it has been corrupted. 
-    The problem is that you don’t know when it could happen and you don’t know what line of source code is causing the crash 
-    (And you can’t check all the code... That would take too much time). 
+    The problem is that you don't know when it could happen and you don't know what line of source code is causing the crash 
+    (And you can't check all the code... That would take too much time). 
     
     Write a comment explaining the steps you will take in order to find the cause of the problem.
 */
